7e269a1fca6ad3ab1f8ec1376af3cf4da9d6667a
Petr Bouchal <pbouchal@gmail.com>
2021-11-25 00:35:32 +0100

    nicer agri plot labels

--- R/tagged-plots.R
+++ R/tagged-plots.R
@@ -68,15 +68,15 @@ make_plot_tagged_agri <- function(agri_tagged) {
     mutate(fond_typ = as_factor(paste0(fond, "\n", typ_podpory)) %>%
              fct_reorder(fin_vyuct_czv, "sum")) %>%
     count(fond_typ, opatreni, climate_share, wt = fin_vyuct_czv/1e9) %>%
-    mutate(climate_share_code = case_when(climate_share == 0 ~ "none",
-                                          climate_share == 0.4 ~ "partial (40%)",
-                                          climate_share == 1 ~ "full (100%)",
-                                          is.na(climate_share) ~ "unknown") %>%
-             fct_relevel("full (100%)", "partial (40%)", "none", "unknown") %>% fct_rev()) %>%
+    mutate(climate_share_code = case_when(climate_share == 0 ~ "No tag",
+                                          climate_share == 0.4 ~ "Partial (40%)",
+                                          climate_share == 1 ~ "Full (100%)",
+                                          is.na(climate_share) ~ "Unknown") %>%
+             fct_relevel("Full (100%)", "Partial (40%)", "No tag", "Unknown") %>% fct_rev()) %>%
     ggplot(aes(n, fond_typ, fill = climate_share_code)) +
     scale_x_continuous(expand = ptrr::flush_axis) +
-    scale_fill_manual(values = c(`full (100%)` = "darkgreen", `partial (40%)` = "lightgreen",
-                                 none = "darkgrey", unknown = "lightgrey"), name = NULL) +
+    scale_fill_manual(values = c(`Full (100%)` = "darkgreen", `Partial (40%)` = "lightgreen",
+                                 `No tag` = "darkgrey", Unknown = "lightgrey"), name = NULL) +
     geom_col() +
     theme_ptrr("x", legend.position = "bottom", legend.key.size = unit(10, "pt")) +
     labs(title = "CAP spending by climate tags",
