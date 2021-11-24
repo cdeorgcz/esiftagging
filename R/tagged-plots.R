58c49171d153db73a7471527f294fa781fdd4166
Petr Bouchal <pbouchal@gmail.com>
2021-11-25 00:32:54 +0100

    add labels to OP overview, closes #5

--- R/tagged-plots.R
+++ R/tagged-plots.R
@@ -1,20 +1,36 @@
 make_plot_tagged_all <- function(esif_tagged_sum) {
-  esif_tagged_sum %>%
-    mutate(climate_share_code = case_when(climate_share == 0 ~ "none",
-                                          climate_share == 0.4 ~ "partial (40%)",
-                                          climate_share == 1 ~ "full (100%)",
-                                          is.na(climate_share) ~ "unknown") %>%
-             fct_relevel("full (100%)", "partial (40%)", "none", "unknown") %>% fct_rev()) %>%
+  data <- esif_tagged_sum %>%
+    mutate(climate_share_code = case_when(climate_share == 0 ~ "No tag",
+                                          climate_share == 0.4 ~ "Partial (40%)",
+                                          climate_share == 1 ~ "Full (100%)",
+                                          is.na(climate_share) ~ "Unknown") %>%
+             fct_relevel("Full (100%)", "Partial (40%)", "None", "Unknown") %>% fct_rev()) %>%
     group_by(climate_share_code) %>%
     count(op_zkr, wt = fin_vyuct_czv/1e9) %>%
     ungroup() %>%
     mutate(op_zkr = as_factor(op_zkr) %>% fct_reorder(n, "sum")) %>%
-    ggplot(aes(y = op_zkr, x = n, fill = climate_share_code)) +
-    scale_fill_manual(values = c(`full (100%)` = "darkgreen", `partial (40%)` = "lightgreen",
-                                 none = "darkgrey", unknown = "lightgrey"), name = NULL) +
-    geom_col() +
+    arrange(op_zkr, desc(climate_share_code)) %>%
+    group_by(op_zkr) %>%
+    mutate(label_pos = cumsum(n),
+           label_value = round(n, 0),
+           label_value = if_else(label_value < 1.5, NA_real_, label_value))
+
+  labels_total <- data %>%
+    ungroup() %>%
+    count(op_zkr, wt = n, name = "total") %>%
+    mutate(label_value = round(total, 0))
+
+  ggplot(data, aes(y = op_zkr)) +
+    scale_fill_manual(values = c(`Full (100%)` = "darkgreen", `Partial (40%)` = "lightgreen",
+                                 `No tag` = "darkgrey", Unknown = "lightgrey"), name = NULL) +
+    geom_col(aes(x = n, fill = climate_share_code)) +
     scale_x_continuous(expand = ptrr::flush_axis) +
     theme_ptrr("x", legend.position = "bottom", legend.key.size = unit(10, "pt")) +
+    geom_text(aes(label = label_value, x = label_pos), hjust = 1.5,
+              colour = "white", family = "IBM Plex Sans") +
+    geom_text(data = labels_total,
+              aes(label = label_value, x = total), hjust = 0, nudge_x = 2,
+              colour = "grey10", family = "IBM Plex Sans") +
     labs(title = "Spending by climate tags",
          subtitle = "bn CZK, all spending. Follows official climate tags.",
          caption = "Payment data for CZ-PL programme unavailable.")
