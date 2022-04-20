2847d85b8490a4d9f8f3e2ffa96fc66ce3c9d2fe
Petr Bouchal <pbouchal@gmail.com>
2022-04-20 12:22:59 +0200

    drop zero category in Sankey

--- R/sankey.R
+++ R/sankey.R
@@ -5,7 +5,8 @@ make_tag_sankey <- function(data) {
     mutate(node = fct_relevel(node, "Full (100%)", "Partial (40%)", "No tag",
                               "Negative (-40%)", "Negative (-100%)") %>% fct_rev(),
            next_node = fct_relevel(next_node, "Full (100%)", "Partial (40%)", "No tag",
-                                   "Negative (-40%)", "Negative (-100%)") %>% fct_rev())
+                                   "Negative (-40%)", "Negative (-100%)") %>% fct_rev()) |>
+    filter(value > 0)
   ggplot(sdf2, aes(x = x,
                    next_x = next_x,
                    node = node,
@@ -14,12 +15,12 @@ make_tag_sankey <- function(data) {
                    fill = node,
                    value = value)) +
     geom_sankey(width = .4,
-                flow.alpha = .6) +
+                flow.alpha = .6, na.rm = TRUE) +
     geom_sankey_text(aes(label = node,
                          colour = after_scale(cr_choose_bw(fill))),
                      type = "sankey",
                      family = "IBM Plex Sans",
-                     size = 3, fill = "gray40") +
+                     size = 3, fill = "gray40", na.rm = TRUE) +
     theme_sankey(base_size = 18, base_family = "IBM Plex Sans") +
     theme(axis.title.x = element_blank()) +
     scale_x_discrete(labels = c("Official", "Manual")) +
